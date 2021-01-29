781202946dcc3d1fe1808ea96f8d83a10b571ba5
tati oliveira <tat1roliv@gmail.com>
2021-01-29 20:15:03 -0300

    imersao1_0

--- alura_quiz/pages/index.js
+++ alura_quiz/pages/index.js
@@ -1,9 +1,21 @@
 import styled from 'styled-components'
 
+
+//criando um componente e puxando o style components
+
 const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+font-size: 50px;
+color: ${({ theme }) => theme.colors.primary};
 `
+
+//function Title(props){ //props /calopsita /var qualquer
+//  return (
+ // <h1>
+  //{props.children}
+  //</h1>
+ // )
+//}
+
 
 export default function Home() {
   return <Title>hola que tal</Title>

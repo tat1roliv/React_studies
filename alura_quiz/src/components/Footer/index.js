4e8f6525476121fd1f736b5d31de8106b470e728
tati oliveira <tat1roliv@gmail.com>
2021-01-30 16:09:56 -0300

    Update index.js

--- alura_quiz/src/components/Footer/index.js
+++ alura_quiz/src/components/Footer/index.js
@@ -33,12 +33,12 @@ export default function Footer(props) {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado durante
+        hola que tal, estudando pra não passar vergonha no estágio :P
         {' '}
-        a
+        
         {' '}
         <a href="https://www.alura.com.br/">
-          <span>Imersão React da Alura</span>
+          <span>Imersão</span>
         </a>
       </p>
     </FooterWrapper>
